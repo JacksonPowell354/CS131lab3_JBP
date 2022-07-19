d049cfd85d8ca288dee329f59941fafca9917b01
jacks <jacks@DESKTOP-H5A26E4.lan>
2022-07-19 00:58:06 -0400

    test

---
+++ CS 131-ON/src/CS131Lab1_JBP/ApplicationSports.java
@@ -0,0 +1,11 @@
+package CS131Lab1_JBP;
+
+public class ApplicationSports {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+BasketballTeam b =new BasketballTeam("hi","bob","marley");
+System.out.println(b.getStats());
+	}
+
+}
