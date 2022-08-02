f004ffb833ca5cdad071fc55cf5ff18e791a8e29
jacks <jacks@DESKTOP-H5A26E4.lan>
2022-08-02 16:10:09 -0400

    testinnnng

---
+++ CS 131-ON/src/CS131Lab2JBP/copy2/Application.java
@@ -0,0 +1,33 @@
+package CS131Lab2JBP.copy2;
+
+public class Application extends Wizard implements Lockable {
+
+	public static void main(String[] args) {
+Wizard jack = new Wizard();
+jack.setKey(2);
+jack.lock(5);
+jack.takeDamage(35);
+System.out.println(jack.getHealth());
+
+jack.unlock(5);
+jack.takeDamage(24);
+System.out.println(jack.getHealth());
+
+jack.takeDamage(65);
+System.out.println(jack.getHealth());
+
+jack.lock(2);
+jack.takeDamage(47);
+System.out.println(jack.getHealth());
+System.out.println(jack.isLocked());
+
+jack.unlock(6);
+System.out.println(jack.isLocked());
+
+jack.setName("Jack");
+System.out.println(jack.toString());
+
+
+	}//end main
+
+}//end class
