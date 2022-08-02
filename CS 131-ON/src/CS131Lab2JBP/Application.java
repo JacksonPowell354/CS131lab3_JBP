4e2d9431795733ad3e3689a84dcb5146838ac86e
jacks <jacks@DESKTOP-H5A26E4.lan>
2022-08-02 14:33:17 -0400

    lab2

---
+++ CS 131-ON/src/CS131Lab2JBP/Application.java
@@ -0,0 +1,11 @@
+package CS131Lab2JBP;
+
+public class Application extends Wizard implements Lockable {
+
+	public static void main(String[] args) {
+Wizard wiz = new Wizard(2,"Harry");
+System.out.println(wiz.toString());
+
+	}//end main
+
+}//end class
